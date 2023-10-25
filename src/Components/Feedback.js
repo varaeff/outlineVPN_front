c613013fbde8fcd62f73a9a1ec0c2d228ce45428
Sergei Varaev <113440374+varaeff@users.noreply.github.com>
2023-10-25 17:05:20 +0700

    some changes

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -6,16 +6,18 @@ function Feedback({ feedback }) {
   return (
     <div className="fdb">
       <div className="topstring">
-        <img className="img" src={feedback.url} alt="avatar"></img>
+        {feedback.url ? (
+          <img className="img" src={feedback.url} alt="avatar"></img>
+        ) : (
+          <div />
+        )}
         <div className="user">
           <b>{feedback.username}</b>
           <div className="rating">
             <b>{feedback.rating}</b>
-            <img width={20} src="star.webp" alt="star" />
           </div>
         </div>
       </div>
-
       <p>{feedback.text}</p>
     </div>
   );
